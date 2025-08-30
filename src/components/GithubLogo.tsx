cc894f19490d70b1add618b56a9444226b9d926a
Jimi <69767677+Jimi-Wilson@users.noreply.github.com>
2025-08-30 11:43:59 +0100

    Refactored github logos to be wrapped in buttons

--- src/components/GithubLogo.tsx
+++ src/components/GithubLogo.tsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, {type ComponentProps} from 'react'
 
 import GithubDarkUrl from '@/assets/github-mark-white.svg?url'
 import GithubLightUrl from '@/assets/github-mark.svg?url'
+import {Button} from "@/components/ui/button.tsx";
 
 
 type GithubLogoProps = {
     url: string;
+    variant: ComponentProps<typeof Button>["variant"];
 }
 
 
+export function GithubLogo({url, variant="outline"}: GithubLogoProps ) {
+    return (
+        <Button variant={variant} size={"icon"} asChild>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+                <img src={GithubLightUrl} alt="GitHub" className="h-6 w-6 block dark:hidden" />
+                <img src={GithubDarkUrl} alt="GitHub" className="h-6 w-6 hidden dark:block" />
+            </a>
+        </Button>
 
-
-export function GithubLogo({url}: GithubLogoProps ) {
-  return (
-        <a href={url}>
-          <img src={GithubLightUrl} alt="GitHub" className="h-6 w-6 block dark:hidden" />
-          <img src={GithubDarkUrl} alt="GitHub" className="h-6 w-6 hidden dark:block" />
-        </a>
-  )
+    )
 }
